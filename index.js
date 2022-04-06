8ed0a9a37f3fac057d2c60ce118cb6f8a7bfa2d8
Tiguyy <102757612+Tiguyy@users.noreply.github.com>
2022-04-06 11:03:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,41 @@ const updateChannel = async () => {
 	    }
 };
 
-client.on("ready", () => {
+client.on('ready', message => {
+  if (message.channel.type != 'text' || message.author.bot)
+    return;
+
+  let command = message.content.split(' ')[0].slice(1);
+  let args = message.content.replace('.' + command, '').trim();
+  let isBotOwner = message.author.id == 'your_user_id';
+
+  switch (command) {
+    case 'restart': {
+      if (!isBotOwner)
+        return;
+
+      message.channel.send('Restarting...').then(m => {
+        client.destroy().then(() => {
+          client.login(process.env.TOKEN);
+        });
+      });
+      break;
+    }
+
+
+    case 'shutdown': {
+      if (!isBotOwner)
+        return;
+
+      message.channel.send('Shutting down...').then(m => {
+        client.destroy();
+      });
+      break;
+    }
+  }
+});
+
+client.on('ready', () => {
 
 	console.log(chalk.green("[BOT] Connected to Discord."));
 	
