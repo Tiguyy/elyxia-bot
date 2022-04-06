47178dd7bf6e774f9386d0b6b9186db99e9b54fd
Tiguyy <102757612+Tiguyy@users.noreply.github.com>
2022-04-06 11:23:49 +0200

    Reset index.js

--- index.js
+++ index.js
@@ -27,38 +27,6 @@ const updateChannel = async () => {
 
 client.login(process.env.TOKEN);
 
-client.on('ready', message => {
-
-  let command = message.content.split(' ')[0].slice(1);
-  let args = message.content.replace('.' + command, '').trim();
-  let isBotOwner = message.author.id == 'Tiguy#5762';
-
-  switch (command) {
-    case 'restart': {
-      if (!isBotOwner)
-        return;
-
-      message.channel.send('Restarting...').then(m => {
-        client.destroy().then(() => {
-          client.login(process.env.TOKEN);
-        });
-      });
-      break;
-    }
-
-
-    case 'shutdown': {
-      if (!isBotOwner)
-        return;
-
-      message.channel.send('Shutting down...').then(m => {
-        client.destroy();
-      });
-      break;
-    }
-  }
-});
-
 client.on('ready', () => {
 
 	console.log(chalk.green("[ÉlyxiBot] Status du serveur Élyxia connecté !"));
