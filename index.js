f16ecf85afb7f1ec4e636eed93a296d6f3df5a5d
Tiguy <102757612+Tiguyy@users.noreply.github.com>
2022-04-06 23:14:16 +0200

    Add commands

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const Discord = require("discord.js");
 const chalk = require('chalk');
 const gamedig = require("gamedig");
 const token = 'process.env.TOKEN';
-const client = new Discord.Client();
+const client = new Discord.Client({
+	intents: [
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_MESSAGES
+	]
+});
 
 client.config = config;
 
@@ -27,6 +32,8 @@ const updateChannel = async () => {
 
 client.login(process.env.TOKEN);
 
+const prefix "!";
+
 client.on('ready', () => {
 
 	console.log(chalk.green("[ÉlyxiaBot] Status du serveur Élyxia connecté !"));
@@ -34,4 +41,32 @@ client.on('ready', () => {
 	updateChannel();
 	setInterval(updateChannel, 60000*0.05);
 	
+});
+
+Client.on("messageCreate", message => {
+	if (message.author.bot) return;
+	
+	if (message.content === prefix + "site"){
+		message.reply("Voici les clés du site : https://elyxiarp.fr/");
+	}
+	
+	else if (message.content === prefix + "boutique"){
+		message.reply("Je t'ouvre la porte à la boutique : https://elyxiarp.fr/");
+	}
+	
+	else if (message.content === prefix + "ip"){
+		message.reply("Voici l'IP du serveur DarkRP, c'est un secret : 51.178.38.4:11111");
+	}
+	
+	else if (message.content === prefix + "help"){
+		message.reply("__**Besoin d'aide ?**__ \n - Pour avoir l'ip : !ip \n - Pour avoir le site : !site \n - Pour avoir la boutique : !boutique \n \n J'espère t'avoir aidé dans ta majestueuse quête !");
+	}
+	
+	else if (message.content === "Les bots sont débils"){
+		message.channel.send("Je te boude...");
+	}
+	
+	else if (message.content = "une blague"){
+		message.channel.send("Qu'est-ce qui est jaune et qui attend ? /n /n Une erreur LUA non résolue ! :rofl:");
+	}
 });
